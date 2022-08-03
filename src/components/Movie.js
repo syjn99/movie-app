6f789feabca4446f84c978be40249156839bd8f4
syjn99 <syjn99@gmail.com>
2022-08-03 20:12:48 +0900

    styling

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
+import style from "./Movie.module.css";
 
 function Movie({ movie }) {
+  if (movie.summary.length === 0) {
+    movie.summary = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dapibus quam aliquet commodo maximus. Etiam rutrum placerat risus a molestie. Quisque non sollicitudin tortor, ac vulputate dolor. Pellentesque cursus ac dolor in mattis. Mauris viverra erat quis venenatis ullamcorper. Nullam a porttitor mauris, eu pellentesque nisi. Aliquam porttitor felis vitae velit facilisis cursus. Ut eget risus semper tortor rhoncus egestas. Integer convallis, ante id imperdiet fringilla, orci felis molestie eros, id gravida elit odio vitae velit. Vivamus cursus tempus elit, sit amet porttitor justo pretium vel. Etiam quis condimentum tortor, in interdum est."
+  }
+  if (movie.genres.length > 4) {
+    movie.genres = movie.genres.slice(0, 4);
+  }
   return (
-    <div >
+    <span className={style.card}>
       <img src={movie.medium_cover_image}></img>
       <h2>
         <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
       </h2>
-      <p>{movie.summary}</p>
       <ul>
         {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
       </ul>
-    </div>);
+      <p>{movie.summary.length > 200 ? movie.summary.slice(0, 200) + "..." : movie.summary}</p>
+    </span>);
 }
 
 export default Movie;
